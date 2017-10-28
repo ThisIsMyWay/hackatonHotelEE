aa37ef3415c802da2e56dbd60178fe3024d6a0ba
ThisIsMyWay <skaczacymuchomor@gmail.com>
2017-10-28 13:46:13 +0200

    added methods to database interface

--- booksy-pax/booksy-pax-backend/src/main/java/pl/mangoteka/db/DataBase.java
+++ booksy-pax/booksy-pax-backend/src/main/java/pl/mangoteka/db/DataBase.java
@@ -1,7 +1,18 @@
 package pl.mangoteka.db;
 
+import pl.mangoteka.db.model.Model;
+
+import java.util.List;
+
 public interface DataBase {
 
 
+    public <T extends Model> List<T> getWholeList(Class<T> clazz);
+
+    public <T extends Model> T getItemById(Class<T> clazzToRetireve, Integer id);
+
+    public <T extends  Model> T persistI(T entity);
+
+    public <T extends  Model> void remove(T entity);
 
 }
